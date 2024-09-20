f48f169dc44c51edcd98164ede8c35de472eb41b
YuriViligiardi <yuriviligiardi01@gmail.com>
2024-09-20 10:34:08 +0200

    Esercizio calcolo tasso di idratazione

---
+++ Es_1_Viligiardi/Es_tasso_idratazione.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tasso di idratazione</title>
+</head>
+<body>
+    <?php
+        $peso = 70;
+        $liquidi = 30;
+        $risultato;
+        echo "<h1> Yuri Viligiardi </h1>";
+        $risultato = $peso * $liquidi;
+        echo "<p>Il tasso di idratazione di una persona che pesa $peso kg è di $risultato</p>"
+    ?>
+</body>
+</html>
